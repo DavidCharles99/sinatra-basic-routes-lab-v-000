a430ca228e41cde5f149fe3504d49ad8281a1e96
David Charles <ratlover03@gmail.com>
2019-09-27 16:07:33 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,9 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-end
+  get '/' do
+    "Hello, World!"
+  end
 
-get '/name' do
-  "My name is _"
+  get '/name' do
+    "My name is Adrienne."
+  end
+
+  get '/hometown' do
+    "My hometown is Atlanta, GA."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Umbilical Moonrise by Lotus."
   end
 end
